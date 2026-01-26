9b71681f638e1be0451dae3861faec5cc8f15460
Javed <nadafjaved92@gmail.com>
2026-01-26 16:54:34 +0530

    Greatestnum_smallestnum

---
+++ fourth_code.js
@@ -0,0 +1,25 @@
+let num = [4,8,7,5,2,44]
+let Greatest = num[0]
+
+for(let i = 1; i < num.length; i++){
+    if(num[i] > Greatest){
+        Greatest = num[i]
+
+    }
+}
+console.log(Greatest)
+
+
+
+
+// let num = [4,8,7,5,2,44]
+// let Smallest = num[0]
+
+// for(let i = 1; i < num.length; i++){
+//     if(num[i] < Smallest){
+//         Smallest = num[i]
+
+    
+//     }
+// }
+// console.log(Smallest)
